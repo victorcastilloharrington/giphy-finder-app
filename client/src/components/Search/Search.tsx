06499195bb01b06c337e626089a8c89d0b36ecd4
victor.castillo.harrington <victor.castillo.harrington@gmail.com>
2024-07-09 20:58:47 -0700

    added list query

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -5,12 +5,13 @@ type Props = {
 
 const Search = ({ search, setSearch }: Props) => {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div className="flex justify-center w-full">
       <input
         type="text"
         placeholder="Search..."
         onChange={(e) => setSearch(e.target.value)}
         value={search}
+        className="border-2 border-gray-300 p-2 w-2/3"
       />
       <button onClick={() => setSearch("")}>X</button>
     </div>
--- client/src/routes/History.tsx
+++ client/src/routes/History.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useQuery } from "@apollo/client";
+import { getSearchHistory } from "../graphql/queries/searchHistory";
 
-type Props = {}
+const History = () => {
+  const { data, error, loading } = useQuery(getSearchHistory);
+  const isLoading = loading && !data;
 
-const History = (props: Props) => {
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
-    <div>History</div>
-  )
-}
+    <div>
+      <table className="table-auto border-1 border-gray-300 w-full">
+        <thead>
+          <tr>
+            <th className="border-2 border-gray-300">Search Term</th>
+            <th className="border-2 border-gray-300">Date Added</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.searchHistoryList?.map((item) => (
+            <tr>
+              <td className="border-2 border-gray-300">{item.queryString}</td>
+              <td className="border-2 border-gray-300">{item.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default History
+export default History;
